abf085624c8560d3a518c33fb7042e4ff680db55
Wes <wesleySalesberry@hotmail.com>
2022-12-14 09:41:28 -0500

    Added routes to the other pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { AboutPage } from "./pages/AboutPage";
 import { IndexPage } from "./pages/Index";
+import { PrivacyPage } from "./pages/PrivacyPage";
+import { ContactPage } from "./pages/ContactPage";
+import { ConfessionPage } from "./pages/ConfessionPage";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/privacy" element={<PrivacyPage />} />
+      <Route path="/contact" element={<ContactPage /> } />
+      <Route path="/:slug/:id" element={<ConfessionPage /> } />
     </Routes>
   );
 }
