e727844845da1bd52b9d69a58445fdfcf641f9b8
Wes <wesleySalesberry@hotmail.com>
2022-12-14 09:44:55 -0500

    Created skel for about page

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,7 +1,13 @@
+import { BaseLayout } from "../components/Layouts/BaseLayout"
+import { Meta } from "../components/metatags"
+
 export const AboutPage = () => {
   return (
-    <>
-      About Page
-    </>
+    <BaseLayout >
+      <Meta
+        title="About The Confessional"
+        url={window.location.href }
+      />
+    </BaseLayout>
   )
 }
