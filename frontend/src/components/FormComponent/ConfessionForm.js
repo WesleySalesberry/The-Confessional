7590833bc1f6d4167745b0006962db274365ed98
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2022-12-15 11:36:02 -0500

    Frontend (#14)
    * Inital layout for git
    
    * Implmented the card to display data
    
    * Test Layout for Index Main Componenet
    
    * Created the base layout for the site
    
    * Created the card layout
    
    * Created the skel for the navbar
    
    * Created the skel for the sidebar
    
    * Created the skel for the pages
    
    * Updated the css
    
    * Added some helpers
    
    * Updated the git ignore
    
    * Updated the Html
    
    * Added routes to the other pages
    
    * Added meta tags
    
    * Added base api calls
    
    * Created the meta tags and title for pages
    
    * Added link to indivial confession
    
    * Created confession main component
    
    * Updated the links in header
    
    * Created skel for about page
    
    * Created main page for confession
    
    * Created skel for Contact page
    
    * Added meta tags component
    
    * Created skel for privacy page
    
    * Backend (#5)
    
    * Frontend (#3)
    
    * Inital layout for git
    
    * Implmented the card to display data
    
    * Test Layout for Index Main Componenet
    
    * Created the base layout for the site
    
    * Created the card layout
    
    * Created the skel for the navbar
    
    * Created the skel for the sidebar
    
    * Created the skel for the pages
    
    * Updated the css
    
    * Added some helpers
    
    * Updated the git ignore
    
    * Updated the Html
    
    * Added routes to the other pages
    
    * Added meta tags
    
    * Added base api calls
    
    * Created the meta tags and title for pages
    
    * Added link to indivial confession
    
    * Created confession main component
    
    * Updated the links in header
    
    * Created skel for about page
    
    * Created main page for confession
    
    * Created skel for Contact page
    
    * Added meta tags component
    
    * Created skel for privacy page
    
    * Updated the git ignore
    
    * Created the main file
    
    * Created the mongodb connection
    
    * Created the controllers
    
    * Created the base model
    
    * Created the base routes
    
    * Added moment to handle time and date
    
    * Update the index path to add opitional params
    
    * Removed strict from index js
    
    * Removed bearer token and updated route
    
    * Created api call class
    
    * Updated Note Card compoenet to add links
    
    * Updated Confession main to added param handling and api calls
    
    * Added api call and updated to handle loading
    
    * Created hooks for aking api call
    
    * Helper to capitalize first letter
    
    * Added style to set height for text area
    
    * Created the form to create the confession
    
    * Created a display component
    
    * Created json for the select component
    
    * Created a card display component
    
    * Cleaned up main and added compoenents
    
    * Added logic to post a confesstion
    
    * Test
    
    * Removed bug

--- frontend/src/components/FormComponent/ConfessionForm.js
+++ frontend/src/components/FormComponent/ConfessionForm.js
@@ -11,7 +11,7 @@ import { usePostConfessions } from '../../Hooks/usePostConfession';
 export const ConfessionForm = () => {
   const [ category, setCategory ] = useState('');
   const [ body, setBody ] = useState('')
-
+  console.log("test")
   const { postConfessions, message, isLoading, error } = usePostConfessions();
 
   const handleSubmit = (evt) => {
@@ -21,12 +21,6 @@ export const ConfessionForm = () => {
 
     setBody('')
     setCategory('')
-  }
-  
-  if(message){
-    return (
-      <h1>{ message }</h1>
-    )
   }
 
   return (
