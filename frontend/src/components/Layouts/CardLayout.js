3b30593608938b556b5155e037591254e98be19c
Wes <wesleySalesberry@hotmail.com>
2022-12-24 13:06:29 -0500

    Fixed bugs and style issues

--- frontend/src/components/Layouts/CardLayout.js
+++ frontend/src/components/Layouts/CardLayout.js
@@ -4,7 +4,7 @@ import Row from "react-bootstrap/esm/Row"
 export const CardLayout = ({ children }) => {
   return (
       <Row
-        className="gx-1 gy-2"
+        className="gx-1 gy-1 w-100"
       >
         { children }
       </Row>
--- frontend/src/components/Navs/HeaderBar.js
+++ frontend/src/components/Navs/HeaderBar.js
@@ -1,12 +1,8 @@
-import { useContext, useState } from "react"
-
 import { LinkContainer } from 'react-router-bootstrap'
 import { FaFacebookSquare, FaTwitterSquare, FaSearch } from "react-icons/fa";
-
 import Container from 'react-bootstrap/esm/Container';
 import Navbar from 'react-bootstrap/esm/Navbar';
 import Nav from 'react-bootstrap/Nav';
-
 import { useNavigate } from "react-router-dom";
 
 export const HeaderBar = () => {
@@ -19,7 +15,50 @@ export const HeaderBar = () => {
   }
 
   return (
-    <Navbar>
+    <Navbar bg="light" expand="lg" className="rounded">
+      <Container fluid>
+        <LinkContainer to="/">
+          <Navbar.Brand className="title">The Confessional</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+           <Nav className="me-auto">
+            <LinkContainer to="/about">
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/privacy">
+              <Nav.Link>Privacy</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav className="ms-auto">
+            <FaFacebookSquare
+              className="nav-icon facebook" 
+            />
+            <FaTwitterSquare
+              className="nav-icon twitter mx-3" 
+            />
+            <FaSearch
+              className="nav-icon me-2" 
+            />
+            <input
+              className="form-control form-control-sm rounded" 
+              type="search"
+              placeholder="Search"
+              aria-label="Search Text"
+              aria-describedby="searchText"
+              onKeyDown={handleKeyDown}
+            /> 
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+{/* <Navbar>
       <Container fluid>
         <LinkContainer to="/">
           <Navbar.Brand className="title">The Confessional</Navbar.Brand>
@@ -54,10 +93,8 @@ export const HeaderBar = () => {
               aria-label="Search Text"
               aria-describedby="searchText"
               onKeyDown={handleKeyDown}
-            />
+            /> 
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
-  )
-}
+    </Navbar> */}
